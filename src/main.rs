18e2e69eb76e6b77c846423d3ee8aa001829c20b
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-03-21 17:44:41 +0545

    Input: set a input string function. Here, we are just setting a skeleton for our game.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//use standard I/O library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number to be rewarded as Genius.");
+    
+    println!("Please input your guess: ");
+    let mut user_guess: String = String::new();
+    io::stdin()
+        .read_line(&mut user_guess);
+    
+    println!("You guessed {user_guess}");
 }
