a4b899f55225c347c81e6094476f538183427277
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-03-22 00:55:42 +0545

    Loop: create a loop to let user keep guessing until the user wins. - Move generated number out of loop so that generated number remains constant.

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,31 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number to be rewarded as Genius.");
 
-    println!("Please input your guess: ");
-    let mut user_guess: String = String::new();
-
-    io::stdin()
-        .read_line(&mut user_guess)
-        .expect("Failed to read user input");
-
     let real_generated_number: u8 = rand::rng().random_range(1..=100);
-    let user_guess: u8 = user_guess.trim().parse().expect("Failed to parse!");
-    println!("You guessed {user_guess}");
+    loop {
+        println!("Please input your guess: ");
+        let mut user_guess: String = String::new();
 
-    match user_guess.cmp(&real_generated_number) {
-        Ordering::Equal => println!("You won! You are officially genius."),
-        Ordering::Less => println!("Try hard genius, your guess is too small."),
-        Ordering::Greater => println!("Try hard genius, your guess is too big."),
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Failed to read user input");
+
+        let user_guess: u8 = match user_guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Failed to parse number, please input valid number. Try again genius.");
+                continue;
+            }
+        };
+        println!("You guessed {user_guess}");
+
+        match user_guess.cmp(&real_generated_number) {
+            Ordering::Equal => {
+                println!("You won! You are officially genius.");
+                break;
+            }
+            Ordering::Less => println!("Try hard genius, your guess is too small."),
+            Ordering::Greater => println!("Try hard genius, your guess is too big."),
+        }
     }
 }
