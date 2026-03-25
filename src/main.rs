80d880b9b163a9a8955f3ca0069ef1d5f8afd5c5
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-03-26 01:16:57 +0545

    Scissors,Paper,Rock: Add new game - Partially implemented - Missing or misaligned blocks - Decision logic pending

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ fn main() {
     println!("Now, choose an option, which game would you like to play?");
     let mut user_game_choice: u8;
     println!("1. Guessing game (1-100)");
+    println!("2. Scissors!, Paper!, Rock!");
+    io::stdin()
+        .read_line(&mut user_game_choice)
+        .expect("Failed to read user input");
+        match user_game_choice{
+        Ok(1) => {
+        println!("---Guessing Game---");
     println!("Guess the number to be rewarded as Genius.");
 
     let real_generated_number: u8 = rand::rng().random_range(1..=100);
@@ -41,4 +48,40 @@ fn main() {
             Ordering::Greater => println!("Try hard genius, your guess is too big."),
         }
     }
+    },
+    Ok(2) => {
+    let play_randomness:u16 = ramd::rng().random_range(1..=1000);
+    let mut play:u8;
+    println!("---Scissors, Paper, Rock");
+    println!("Beat the randomness and you'll be untouchable by fate!");
+    if(play_randomness<=1000){
+        play=1; //Scissors
+        let play_name:String = "Scissors";
+    }
+    else if (play_randomness>1000 && play_randomness<=2000){
+        play=2; //Paper
+        let play_name:String = "Paper";
+        }
+    else{
+        play=3; //Rock
+        let play_name:String = "Rock";
+        }
+        println!("Now, choose your play.");
+        println!("1. Scissors, 2.Paper, 3.Rock");
+        let mut user_play:u8;
+        io::stdin()
+            .read_line(&mut user_play)
+                .expect("Failed to read user input");
+        
+        if(user_play==1){
+        println!("You choose Scissors.");
+        println!("AI(Aayam Intelligence choose 
+    }
+    else if(user_play==2){
+        println!("You choose Paper.");
+    }
+    else if(user_play==3){
+        println!("You choose Rock.");
+    }
+}
 }
